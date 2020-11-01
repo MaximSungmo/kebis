ed8446e57cd455baaa7a521d1d789b5f742641cc
MaximSungmo <ksm5318@naver.com>
2020-11-01 19:56:50 +0900

    완

--- api/common.js
+++ api/common.js
@@ -1,11 +1,11 @@
-function setCookie(cookieName, value, exdays){
+function setCookie(cookieName, value, exdays) {
     var exdate = new Date();
     exdate.setDate(exdate.getDate() + exdays);
-    var cookieValue = escape(value) + ((exdays==null) ? "" : "; expires=" + exdate.toGMTString());
+    var cookieValue = escape(value) + ((exdays == null) ? "" : "; expires=" + exdate.toGMTString());
     document.cookie = cookieName + "=" + cookieValue;
 }
 
-function deleteCookie(cookieName){
+function deleteCookie(cookieName) {
     var expireDate = new Date();
     expireDate.setDate(expireDate.getDate() - 1);
     document.cookie = cookieName + "= " + "; expires=" + expireDate.toGMTString();
@@ -16,10 +16,10 @@ function getCookie(cookieName) {
     var cookieData = document.cookie;
     var start = cookieData.indexOf(cookieName);
     var cookieValue = '';
-    if(start != -1){
+    if (start != -1) {
         start += cookieName.length;
         var end = cookieData.indexOf(';', start);
-        if(end == -1)end = cookieData.length;
+        if (end == -1) end = cookieData.length;
         cookieValue = cookieData.substring(start, end);
     }
     return unescape(cookieValue);
@@ -30,20 +30,34 @@ function logout() {
     window.location.reload();
 }
 
+function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+    return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
+
+
 (function () {
     const userApi = new UserApi();
     const id = sessionStorage.getItem('id')
     userApi.existsUsers({id: id}).done(json => {
-        if(json.result) {
+        if (json.result) {
             $(".login_before").hide();
             $(".login_after").show();
-            $("#write").show();
             console.log("auth")
         } else {
             $(".login_before").show();
             $(".login_after").hide();
-            $("#write").hide();
             console.log("no auth")
         }
     });
+    if(id==="admin"){
+        $("#write").show();
+        $("#save_btn").show();
+    } else {
+        $("#write").hide();
+        $("#save_btn").hide();
+    }
 })()
